a00d9f716a28464b35d23f859f14e946147eb0b1
Benjamin Bernard <bartowsky5@live.com>
2023-05-31 23:57:15 +0200

    Frontend : issue #31 - Correction envoi vers la BDD

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 
 const corsOptions = {
-  origin: "http://localhost:3078/",
+  origin: "http://localhost:3078",
   optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions))
