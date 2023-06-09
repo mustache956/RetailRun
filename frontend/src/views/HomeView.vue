b6bec32599ac49e33804d4434eccc6e1b9d7399c
Blackstar01 <vitalwarren@yahoo.com>
2023-06-09 10:00:09 +0200

    redirect admin

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,7 +5,7 @@
                 <img src="../assets/LogoRetailRun.png" alt="RETAIL RUN" width="244">
             </div>
             <div id="btns">
-                <button @click="this.$router.push('/')" class="button">
+                <button @click="this.$router.push('/admin')" class="button">
                     Rechercher un produit
                 </button>
                 <button @click="this.$router.push('/signalProblem')" class="button">
