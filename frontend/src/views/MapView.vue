297921e479fd953ec19226daa2a353f24d290a6c
mustache956 <antoine.rincheval@gmail.com>
2023-06-28 15:46:42 +0200

    implémentation de l'algo a* et affichage du chemin

---
+++ frontend/src/views/MapView.vue
@@ -0,0 +1,19 @@
+<template>
+    <Map>
+    </Map>
+</template>
+  
+<script>
+    import Map from '@/components/Map.vue'
+
+    export default {
+        name: 'MapView',
+        components: {
+            Map
+        }
+    }
+</script>
+
+<style>
+
+</style>
